717b59c774e3341eb177b33daa4f011a299bfa8a
wenyujie <wenyujie@hundun.cn>
2020-12-23 11:29:04 +0800

    chore: update

--- debounce/index.js
+++ debounce/index.js
@@ -2,25 +2,27 @@
  * @Author: wenyujie
  * @LastEditors: wenyujie
  * @Date: 2020-12-22 16:44:34
- * @LastEditTime: 2020-12-22 16:47:48
+ * @LastEditTime: 2020-12-22 17:38:39
  * @Description: 防抖
  * @FilePath: /debounce/index.js
  * @powerd by hundun
  */
 
 //  在n秒内重新触发，会重新开始计算时间
-function debounce(fn, time) {
+function debounce(fn, timeout) {
+  timeout = timeout || 300;
   let timer = null;
-  return () => {
+  return function (...args) {
+    var _this = this;
     clearTimeout(timer);
     timer = setTimeout(() => {
-      fn();
-    }, time);
+      fn.apply(_this, args);
+    }, timeout);
   };
 }
 
 function sayDebounce() {
-    console.log("我是防抖，每次触发我都会重新计算一次时间")
+  console.log("我是防抖，每次触发我都会重新计算一次时间");
 }
 
-document.onclick = debounce(sayDebounce, 1000)
+document.onclick = debounce(sayDebounce, 1000);
