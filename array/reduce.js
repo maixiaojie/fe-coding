e38658729d76c827013cd20b01860b1a2993ac75
wenyujie <wenyujie@hundun.cn>
2020-12-28 10:36:29 +0800

    chore: reduce

--- array/reduce.js
+++ array/reduce.js
@@ -2,7 +2,7 @@
  * @Author: wenyujie
  * @LastEditors: wenyujie
  * @Date: 2020-12-21 17:30:55
- * @LastEditTime: 2020-12-22 15:32:52
+ * @LastEditTime: 2020-12-28 10:36:00
  * @Description: reduce相关实现
  * @FilePath: /array/reduce.js
  * @powerd by wenyujie
@@ -21,6 +21,7 @@ Array.prototype.reduce = function (callback, prev) {
       prev = callback(prev, this[i], i, this);
     }
   }
+  return prev;
 };
 /**
  * @description: reduce实现map
@@ -47,3 +48,34 @@ Array.prototype.reduceToFilter = function (handler) {
     return target;
   }, []);
 };
+
+
+/**
+ * 将array转为下面的格式
+ * { s1: [ 'rule1' ], s2: [ 'rule2', 'rule3' ] }
+ */
+
+var array = [
+  {
+    selector: "s1",
+    rules: "rule1",
+  },
+  {
+    selector: "s2",
+    rules: "rule2",
+  },
+  {
+    selector: "s2",
+    rules: "rule3",
+  },
+];
+
+function transform(arr, key, value) {
+  return arr.reduce(function (pre, item) {
+    pre[item[key]] = (pre[item[key]] || []).concat(item[value]);
+    return pre;
+  }, {});
+}
+
+var tree = transform(array, "selector", "rules");
+console.log(tree);
