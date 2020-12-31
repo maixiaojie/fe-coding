ce2349569cd916bb86aed3e99f5905cab85d496d
wenyujie <wenyujie@hundun.cn>
2020-12-31 14:28:14 +0800

    chore:deepClone

--- deep_clone/index.js
+++ deep_clone/index.js
@@ -2,28 +2,33 @@
  * @Author: wenyujie
  * @LastEditors: wenyujie
  * @Date: 2020-12-25 15:18:00
- * @LastEditTime: 2020-12-25 15:25:23
+ * @LastEditTime: 2020-12-30 18:44:16
  * @Description: 深拷贝
  * @FilePath: /deep_clone/index.js
  * @powerd by wenyujie
  */
 
-function deepClone(target) {
-  if (typeof target !== "object") return target;
-  let obj;
-  if (Array.isArray(target)) {
-    obj = [];
-  } else {
-    obj = {};
-  }
-  for (let prop in target) {
-    if (target.hasOwnProperty(prop)) {
-      obj[prop] = deepClone(target[prop]);
-    } else {
-      obj[prop] = target[prop];
+function isObject(obj) {
+  return typeof obj === "object" && obj != null;
+}
+function deepClone(source) {
+  if (!isObject(source)) return source;
+  var target = Array.isArray(source) ? [] : {};
+
+  for (var key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      if (isObject(source[key])) {
+        target[key] = deepClone(source[key]);
+      } else {
+        target[key] = source[key];
+      }
     }
   }
-  return obj;
+  return target;
 }
-
-console.log(deepClone({ a: 1, b: 2, v: { d: 2 } }));
+var source = {
+  a: "1",
+  b: undefined,
+  c: new Date(),
+};
+console.log(deepClone(source));
