6a226db1236d4cae33ad430b338f7f62d953b4b7
wenyujie <wenyujie@hundun.cn>
2021-01-15 15:23:01 +0800

    chore: 循环引用

--- deep_clone/index.js
+++ deep_clone/index.js
@@ -2,7 +2,7 @@
  * @Author: wenyujie
  * @LastEditors: wenyujie
  * @Date: 2020-12-25 15:18:00
- * @LastEditTime: 2020-12-30 18:44:16
+ * @LastEditTime: 2021-01-15 15:22:39
  * @Description: 深拷贝
  * @FilePath: /deep_clone/index.js
  * @powerd by wenyujie
@@ -31,4 +31,29 @@ var source = {
   b: undefined,
   c: new Date(),
 };
+
 console.log(deepClone(source));
+
+/**
+ * 解决循环引用的问题，用一个数组进行保存对象，如果已经有了，直接用。
+ */
+
+
+function deepClone1(initalObj, finalObj) {
+  var obj = finalObj || {};
+  for (let i in initalObj) {
+    var prop = initalObj[i];
+    if (prop === obj) {
+      continue;
+    }
+    if (typeof prop === "object") {
+      obj[i] = prop.constructor === Array ? [] : {};
+      arguments.callee(prop, obj[i]);
+    } else {
+      obj[i] = prop;
+    }
+  }
+  return obj;
+}
+
+console.log(deepClone1(source))
